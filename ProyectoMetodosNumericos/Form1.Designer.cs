b9e0ee2ec8f0b1b6ef685b1ebe24439a5321dd72
Stiwar0098 <brayan98macas@gmail.com>
2021-04-08 19:58:52 -0500

    gbfvd

--- ProyectoMetodosNumericos/Form1.Designer.cs
+++ ProyectoMetodosNumericos/Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace ProyectoMetodosNumericos
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(529, 259);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
