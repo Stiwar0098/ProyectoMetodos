af41bf5c70e4bb8db6ad350a25f6fef736a462be
elian-doctom <elianRAMIER2017@hotmail.com>
2021-04-13 10:16:25 -0500

    q

--- ProyectoMetodosNumericos/Formularios/frmGaussLegendre.designer.cs
+++ ProyectoMetodosNumericos/Formularios/frmGaussLegendre.designer.cs
@@ -171,11 +171,10 @@
             "13",
             "14",
             "15"});
-            this.cmbCifrasSignif.Location = new System.Drawing.Point(544, 29);
+            this.cmbCifrasSignif.Location = new System.Drawing.Point(511, 31);
             this.cmbCifrasSignif.Name = "cmbCifrasSignif";
             this.cmbCifrasSignif.Size = new System.Drawing.Size(51, 26);
             this.cmbCifrasSignif.TabIndex = 4;
-            this.cmbCifrasSignif.SelectedIndexChanged += new System.EventHandler(this.cmbCifrasSignif_SelectedIndexChanged);
             // 
             // label11
             // 
@@ -184,9 +183,9 @@
             this.label11.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.Location = new System.Drawing.Point(344, 34);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(194, 20);
+            this.label11.Size = new System.Drawing.Size(161, 20);
             this.label11.TabIndex = 34;
-            this.label11.Text = "Cantidad de decimales =\r\n";
+            this.label11.Text = "Cifras Significativas =\r\n";
             // 
             // label9
             // 
