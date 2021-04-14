fb36f533f7f165b50c6f4f6fd9a206738e9d4852
elian-doctom <elianRAMIER2017@hotmail.com>
2021-04-14 01:16:21 -0500

    cvb

--- ProyectoMetodosNumericos/formularios/frmGaussLegendre.designer.cs
+++ ProyectoMetodosNumericos/formularios/frmGaussLegendre.designer.cs
@@ -339,7 +339,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(880, 345);
+            this.ClientSize = new System.Drawing.Size(880, 562);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnAplicarMetodo);
             this.Controls.Add(this.btnNuevo);
