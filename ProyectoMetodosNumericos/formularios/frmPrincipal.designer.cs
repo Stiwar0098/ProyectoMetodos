63cfc3d2332ba3541e90eff06951f45daefdfb72
Stiwar0098 <brayan98macas@gmail.com>
2021-04-14 02:14:33 -0500

    maximizar desactivado

--- ProyectoMetodosNumericos/formularios/frmPrincipal.designer.cs
+++ ProyectoMetodosNumericos/formularios/frmPrincipal.designer.cs
@@ -172,6 +172,7 @@
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(900, 610);
             this.Name = "frmPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
